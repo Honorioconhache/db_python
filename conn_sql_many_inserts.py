dc0427ed122f1d101e59e8c6a7d3c66b67b6c68f
Honorius di Gesus <Jaramilloortega@gmail.com>
2023-01-24 23:41:03 -0500

    Ingresamos varios productos a la DB

--- conn_sql_many_inserts.py
+++ conn_sql_many_inserts.py
@@ -4,13 +4,20 @@ conn = sqlite3.connect('myBBDD')
 puntero = conn.cursor()
 #puntero.execute("CREATE TABLE PRODUCTOS(NARTICULO VARCHAR(50), PRECIO INTEGER, SECCION VARCHAR(20))")
 
+muchos_productos = [
+    ("Guayos",100,"Deportes"),
+    ("Camisa",150,"Ejecutivo"),
+    ("Cadena",1000,"Joyas")
+]
+
 try:
-    puntero.execute("INSERT INTO PRODUCTOS VALUES('CAMISETA', 100, 'MODA')")
-    print('DATOS INGRESADOS CORRECTAMENTE BD')
+    puntero.executemany("INSERT INTO PRODUCTOS VALUES (?,?,?)", muchos_productos)
     conn.commit()
     puntero.close()
 except:
   print('FALLO')
   puntero.close()
+
+
 
 conn.close()
